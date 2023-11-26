7cb77ed9c7ed64ce467c73bae6251189ec77690d
RyanMirandaBarbosa <ryan.barbosa@sptech.school>
2023-11-26 14:57:02 -0300

    Terminando estrutura HTML e CSS das paginas Login, Cadastro e Sobre o Projeto. Adicionando Páginas: Perfil, Fórum, Conhecimento, Catálogo de Espécies. E pasta com modelos de modals: Criar public, Menu, Pesquisa e Responder public. Atualizando CSS & JS das páginas para exibir e fechar as modals.

---
+++ Site/Public/JS/JS.js
@@ -0,0 +1,34 @@
+// abrir e fechar menu
+function exibir_menu() {
+    menu.style.display = 'flex';
+}
+
+function fechar_menu() {
+    menu.style.display = 'none';
+}
+
+// perfil
+function exibir_pesquisa1() {
+    pesquisa1.style.display = 'flex';
+}
+
+function exibir_pesquisa2() {
+    pesquisa2.style.display = 'flex';
+}
+
+function fechar_pesquisa1() {
+    pesquisa1.style.display = 'none';
+}
+
+function fechar_pesquisa2() {
+    pesquisa2.style.display = 'none';
+}
+
+// fórum 
+function criar_publicacao() {
+    criar_public.style.display = 'flex';
+}
+
+function fechar_criar_public() {
+    criar_public.style.display = 'none';
+}
